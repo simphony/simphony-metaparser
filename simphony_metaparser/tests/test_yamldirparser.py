1708df3274c6931c64e046a686cf60409bd02f45
Stefano Borini <sborini@enthought.com>
2017-01-26 18:27:11 +0000

    Added test to the returned ontology

--- simphony_metaparser/tests/test_yamldirparser.py
+++ simphony_metaparser/tests/test_yamldirparser.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from simphony_metaparser.exceptions import ParsingError
+from simphony_metaparser.nodes import Ontology
 from simphony_metaparser.yamldirparser import \
     check_name_clash, YamlDirParser
 
@@ -15,7 +16,20 @@ class TestYamlDirParser(unittest.TestCase):
 
     def test_full_parsing(self):
         parser = YamlDirParser()
-        nodes = parser.parse(self.yamldir)
+        ontology = parser.parse(self.yamldir)
+        self.assertIsInstance(ontology, Ontology)
+
+        self.assertEqual(len(ontology.data_types), 117)
+
+        root_cuds = ontology.root_cuds_item
+
+        self.assertEqual(root_cuds.name, "CUBA.CUDS_ITEM")
+        self.assertIsNone(root_cuds.parent)
+        self.assertEqual(len(root_cuds.children), 7)
+        self.assertEqual(len(root_cuds.property_entries), 3)
+
+        for child in root_cuds.children:
+            self.assertIs(child.parent, root_cuds)
 
     def test_check_name_clash(self):
         with self.assertRaises(ParsingError):
